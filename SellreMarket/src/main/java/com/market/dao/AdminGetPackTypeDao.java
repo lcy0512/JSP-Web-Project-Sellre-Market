5226f02ed21d3ee1aaca118b93ab26b579a83dbd
shin <nas221@naver.com>
2024-02-07 16:04:32 +0900

    2024.02.07 snr : 관리자 제품등록 - 포장타입 가져오기

---
+++ SellreMarket/src/main/java/com/market/dao/AdminGetPackTypeDao.java
@@ -0,0 +1,69 @@
+package com.market.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import com.market.dto.AdminGetCategoryDto;
+import com.market.dto.AdminGetPackTypeDto;
+
+public class AdminGetPackTypeDao {
+
+	DataSource dataSource;
+		
+	/************************************************************************************************
+	 * Function : context.xml에 설정한 db이름을 가져온다. 
+	 * @param 	: null
+	 * @return 	: null
+	************************************************************************************************/
+	public AdminGetPackTypeDao() {
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/sellreMarket");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	/************************************************************************************************
+	 * Function : 포장타입 조회
+	 * @param 	: null
+	 * @return 	: ArrayList
+	************************************************************************************************/	
+	
+	public ArrayList<AdminGetPackTypeDto> selectPackType() {
+		ArrayList<AdminGetPackTypeDto> dtos = new ArrayList<AdminGetPackTypeDto>();
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			conn = dataSource.getConnection();
+			String query = "SELECT packtype FROM packing group by packtype";
+		
+			ps = conn.prepareStatement(query);
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				AdminGetPackTypeDto pack = new AdminGetPackTypeDto();
+				pack.setPacktype(rs.getString(1));
+				dtos.add(pack);
+			}
+			
+			conn.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return dtos;
+	}
+	
+}
